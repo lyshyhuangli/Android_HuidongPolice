0819bc7d05ff93861ad5dc06296a4968bbd08fd7
lyshyhuangli <lyshyhuangli@126.com>
2018-01-12 20:24:00 +0800

    1-12

--- app/src/main/java/com/huizhou/huidongpolice/utils/ServerConfig.java
+++ app/src/main/java/com/huizhou/huidongpolice/utils/ServerConfig.java
@@ -6,13 +6,13 @@ package com.huizhou.huidongpolice.utils;
 
 public interface ServerConfig
 {
-    public final  static String  SERVER_IP = "202.96.142.210";
+    public final  static String  SERVER_IP = "127.0.0.1";
 
-    public final  static String  SERVER_PORT = "8082";
+    public final  static String  SERVER_PORT = "38082";
 
-    public final static String  RESTFUL_API_IP = "202.96.142.210";
+    public final static String  RESTFUL_API_IP = "127.0.0.1";
 
-    public final static String  RESTFUL_API_PORT = "8081";
+    public final static String  RESTFUL_API_PORT = "38081";
 
     public final static String FILE_SAVE_PATH ="sdcard/Download/huidongpolice/";
 }
